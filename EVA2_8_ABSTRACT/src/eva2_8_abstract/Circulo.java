7dc3b3ee752fcb72314f993631c802b05da024c1
DavidViverotec2 <L25550861@chihuahua2.tecnm.mx>
2026-03-24 10:18:52 -0600

    Add files via upload

---
+++ EVA2_8_ABSTRACT/src/eva2_8_abstract/Circulo.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package eva2_8_abstract;
+
+/**
+ *
+ * @author elvat
+ */
+public class Circulo extends Figuras{
+    private double radio;
+
+    public Circulo() {
+        radio = 0;
+    }
+
+    public Circulo(double radio) {
+        this.radio = radio;
+    }
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+
+    @Override
+    public double calcularArea() {
+        return Math.PI * Math.pow(radio, 2);
+    }
+
+    @Override
+    public double calcularPerimetro() {
+        return 2 * Math.PI * radio;
+    }
+    
+    
+}
---
+++ EVA2_8_ABSTRACT/src/eva2_8_abstract/Figuras.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package eva2_8_abstract;
+
+/**
+ *
+ * @author elvat
+ */
+abstract public class Figuras {
+    
+    // Una clase abstracta no permite crear objetos
+    // Una clase abstracta puede tener cero o mas metodos
+    // Abstractos
+    // Un metodo abstracto es un metodo si implementacion
+    
+    abstract public double calcularArea();
+    abstract public double calcularPerimetro();
+}
