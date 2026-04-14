75367870783f576a60bdfd0161e24d8d31edcede
DavidViverotec2 <L25550861@chihuahua2.tecnm.mx>
2026-04-14 00:17:01 -0600

    Add files via upload

---
+++ EVA2_12_INTERFACES/src/eva2_12_interfaces/EVA2_12_INTERFACES.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_12_interfaces;
+
+/**
+ *
+ * @author elvat
+ */
+public class EVA2_12_INTERFACES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Empleado empleado = new Empleado("Maquilero", 10000, "Hernan", "Cortez", 56);
+        empleado.mostrarMensaje();
+    }
+    
+}
